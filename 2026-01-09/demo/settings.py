3d6d93f41dd2a02090081b1512312cfef5ff418e
AnittaMariya99 <anittamariyavs@gmail.com>
2026-01-09 15:14:08 +0530

    new data

---
+++ 2026-01-09/demo/settings.py
@@ -0,0 +1,54 @@
+
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s:%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+# basic details
+PROJECT = "classiccars"
+CLIENT_NAME = ""
+PROJECT_NAME = "classiccars"
+FREQUENCY = ""
+BASE_URL = "https://classiccars.com/listings/find"
+
+
+
+
+FILE_NAME = f"classiccars.csv"
+FILE_HEADERS = ["make","model","year","vin","price","mileage","transmission","engine","color","fuel type","body style","description","image urls","url"]
+
+# Mongo db and collections
+# MONGO_URI = "mongodb://localhost:27017/"
+# MONGO_HOST = "localhost"
+# MONGO_PORT = 27017
+MONGO_DB = "classiccars_db"
+MONGO_URI = "mongodb://mongotraining:a4892e52373844dc4862e6c468d11b6df7938e16@167.172.244.21:27017/?authSource=admin"
+
+
+
+MONGO_COLLECTION_RESPONSE = f"{PROJECT_NAME}_url"
+MONGO_COLLECTION_CATEGORY = f"{PROJECT_NAME}_category_url"
+MONGO_COLLECTION_URL_FAILED = f"{PROJECT_NAME}_url_failed"
+MONGO_COLLECTION_DATA = f"{PROJECT_NAME}_data"
+
+
+
+HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'no-cache',
+    'pragma': 'no-cache',
+    'priority': 'u=0, i',
+    'sec-ch-ua': '"Not_A Brand";v="99", "Chromium";v="142"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+}
