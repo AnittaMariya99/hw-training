6c1730770ccedf38367ad4c31fd0125841c8bb29
AnittaMariya99 <anittamariyavs@gmail.com>
2026-02-11 18:05:06 +0530

    commit

---
+++ 2026-02-11/ah/settings.py
@@ -0,0 +1,31 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s:%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+# basic details
+PROJECT = "ah.nl"
+CLIENT_NAME = ""
+PROJECT_NAME = "ah.nl"
+FREQUENCY = ""
+CATEGORY_URL= "https://www.ah.nl/producten"
+BASE_URL = "https://www.ah.nl"
+
+
+
+FILE_NAME = f"ah_nl_2026_02_11_sample.csv"
+
+# Mongo db and collections
+MONGO_URI = "mongodb://mongotraining:a4892e52373844dc4862e6c468d11b6df7938e16@209.97.183.63:27017/?authSource=admin"
+MONGO_DB = "ah_nl_db"
+
+
+
+MONGO_COLLECTION_RESPONSE = f"{PROJECT_NAME}_url"
+MONGO_COLLECTION_CATEGORY = f"{PROJECT_NAME}_category_url"
+MONGO_COLLECTION_PRODUCTS = f"{PROJECT_NAME}_products_url"
+MONGO_COLLECTION_URL_FAILED = f"{PROJECT_NAME}_url_failed"
+MONGO_COLLECTION_DATA = f"{PROJECT_NAME}_data"
